c7c18c5eedc16655f93043977c884621b76b7dc8
Rodrigo Mendes Brocchi <rodrigo.brocchi@gmail.com>
2024-07-30 09:35:07 -0300

    config: swagger url default

--- src/main/java/br/com/ezblue/ezclientservices/EzClientServicesApplication.java
+++ src/main/java/br/com/ezblue/ezclientservices/EzClientServicesApplication.java
@@ -3,26 +3,36 @@ package br.com.ezblue.ezclientservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 public class EzClientServicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EzClientServicesApplication.class, args);
     }
 
-    //TODO: ++Ajuste necessário nas Keys do veiculo, transformar a placa do carro como primaryKey,
+    // TODO: ++Ajuste necessário nas Keys do veiculo, transformar a placa do carro
+    // como primaryKey,
     // e alterar as consultas para placa do carro e não pelo o ID do veiculo
 
-    //TODO: +Histórico de Transações: Registrar e consultar histórico de transações de estacionamento.
-    //TODO: +Notificações e Alertas: Enviar notificações aos clientes sobre expiração de tempo de estacionamento,
+    // TODO: +Histórico de Transações: Registrar e consultar histórico de transações
+    // de estacionamento.
+    // TODO: +Notificações e Alertas: Enviar notificações aos clientes sobre
+    // expiração de tempo de estacionamento,
     // pagamentos, etc.(pode ser futuramente transferido para um serviço dedicado).
 
-
-    //TODO: -Integração com Serviços de Pagamento: Interface para integrar com serviços de pagamento externos para
-    // recarga de contas e pagamento de tarifas. (pode ser futuramente transferido para um serviço dedicado).
-    //TODO: -Autenticação e Autorização: Gerenciamento de autenticação e autorização (pode ser futuramente
+    // TODO: -Integração com Serviços de Pagamento: Interface para integrar com
+    // serviços de pagamento externos para
+    // recarga de contas e pagamento de tarifas. (pode ser futuramente transferido
+    // para um serviço dedicado).
+    // TODO: -Autenticação e Autorização: Gerenciamento de autenticação e
+    // autorização (pode ser futuramente
     // transferido para um serviço dedicado).
-    //TODO: -Relatórios e Estatísticas: Geração de relatórios e estatísticas de uso para os clientes. (pode ser
+    // TODO: -Relatórios e Estatísticas: Geração de relatórios e estatísticas de uso
+    // para os clientes. (pode ser
     // futuramente transferido para um serviço dedicado).
 
 }
