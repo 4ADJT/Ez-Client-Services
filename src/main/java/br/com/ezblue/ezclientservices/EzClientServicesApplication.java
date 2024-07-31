4e4187e07b465d6d7c6228d611c5f7c2c5ee475c
Rodrigo Mendes Brocchi <rodrigo.brocchi@gmail.com>
2024-07-30 22:23:02 -0300

    doc: remove comments

--- src/main/java/br/com/ezblue/ezclientservices/EzClientServicesApplication.java
+++ src/main/java/br/com/ezblue/ezclientservices/EzClientServicesApplication.java
@@ -14,25 +14,4 @@ public class EzClientServicesApplication {
         SpringApplication.run(EzClientServicesApplication.class, args);
     }
 
-    // TODO: ++Ajuste necessário nas Keys do veiculo, transformar a placa do carro
-    // como primaryKey,
-    // e alterar as consultas para placa do carro e não pelo o ID do veiculo
-
-    // TODO: +Histórico de Transações: Registrar e consultar histórico de transações
-    // de estacionamento.
-    // TODO: +Notificações e Alertas: Enviar notificações aos clientes sobre
-    // expiração de tempo de estacionamento,
-    // pagamentos, etc.(pode ser futuramente transferido para um serviço dedicado).
-
-    // TODO: -Integração com Serviços de Pagamento: Interface para integrar com
-    // serviços de pagamento externos para
-    // recarga de contas e pagamento de tarifas. (pode ser futuramente transferido
-    // para um serviço dedicado).
-    // TODO: -Autenticação e Autorização: Gerenciamento de autenticação e
-    // autorização (pode ser futuramente
-    // transferido para um serviço dedicado).
-    // TODO: -Relatórios e Estatísticas: Geração de relatórios e estatísticas de uso
-    // para os clientes. (pode ser
-    // futuramente transferido para um serviço dedicado).
-
 }
